1f84611d49bc979529d0a7cca017d32a937effb6
Sam Kinard <snkinard@gmail.com>
2016-08-08 16:10:37 -0500

    install pip in ubuntu

--- scripts/setup_python_3.sh
+++ scripts/setup_python_3.sh
@@ -3,6 +3,7 @@
 # install python 3
 if [ "$(uname -s)" = "Linux" ]; then
   sudo apt-get install python3
+  sudo apt-get install pip
 elif [ "$(uname -s)" = "Darwin" ]; then
   brew install python3
 fi
