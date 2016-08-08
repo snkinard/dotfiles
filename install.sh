35672c42cd387473bc636134032e51b417ab47bf
Sam Kinard <snkinard@gmail.com>
2016-08-08 11:25:10 -0500

    nevermind lets not use linuxbrew. keep it simple, stupid

--- install.sh
+++ install.sh
@@ -5,36 +5,30 @@ if [ "$(uname -s)" == "Linux" ]; then
 
   sudo apt-get update
 
-  # linux-brew dependencies
-  sudo apt-get install make
-  sudo apt-get install gawk
-  sudo apt-get install ruby
+  # dependencies
   sudo apt-get install git
-
-  # install linuxbrew
-  ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Linuxbrew/install/master/install)"
-
-  export PATH="$HOME/.linuxbrew/bin:$PATH"
-  export MANPATH="$HOME/.linuxbrew/share/man:$MANPATH"
-  export INFOPATH="$HOME/.linuxbrew/share/info:$INFOPATH"
-
-  # install other dependencies with brew
-  brew install vim
-  brew install zsh
-
-  # make zsh the default shell
-  chsh -s $(which zsh)
-
+  sudo apt-get install vim
+  sudo apt-get install zsh
 
 elif [ "$(uname -s)" == "Darwin" ]; then
   echo "Installing dependencies for Mac OS"
+  
+  # make sure homebrew is installed
+  /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)" 
+
+  brew install git
+  brew install vim
+  brew install zsh
+fi
+
+if [ $(echo $SHELL) != "bin/zsh" ]
+  # make zsh the default shell
+  chsh -s $(which zsh)
 fi
 
 # git that shit
 cd ~
 git clone https://github.com/snkinard/dotfiles.git
 
-cd dotfiles
 ~/dotfiles/bootstrap.sh
-
 
