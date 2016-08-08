c8438af0dd39f8e799e27410a46d668de7adb970
Sam Kinard <snkinard@gmail.com>
2016-08-08 16:20:14 -0500

    no sudo for pip

--- scripts/setup_python_3.sh
+++ scripts/setup_python_3.sh
@@ -5,8 +5,8 @@ if [ "$(uname -s)" = "Linux" ]; then
   sudo apt-get install python3
   sudo apt-get install python-pip
   sudo pip install --upgrade pip
-  sudo pip install --upgrade virtualenv
-  sudo pip install --upgrade virtualenvwrapper
+  pip install --upgrade virtualenv
+  pip install --upgrade virtualenvwrapper
 elif [ "$(uname -s)" = "Darwin" ]; then
   brew install python3
   pip install --upgrade virtualenv
