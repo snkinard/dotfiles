fddc45164ead1ded5fd2cae48523a451e97b9468
Sam Kinard <snkinard@gmail.com>
2016-08-08 16:36:30 -0500

    install python 3.5. make sure bootstrap can update via git

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
+cd "$(dirname "${BASH_SOURCE}")";
 
 git pull origin master;
 git submodule update --init antigen
--- scripts/setup_python_3.sh
+++ scripts/setup_python_3.sh
@@ -2,11 +2,15 @@
 
 # install python 3
 if [ "$(uname -s)" = "Linux" ]; then
-  sudo apt-get install python3
+  sudo add-apt-repository ppa:fkrull/deadsnakes
+  sudo apt-get update
+  sudo apt-get install python3.5
   sudo apt-get install python-pip
+  sudo add-apt-repository --remove ppa:fkrull/deadsnakes
+
   sudo pip install --upgrade pip
-  pip install --upgrade virtualenv
-  pip install --upgrade virtualenvwrapper
+  sudo pip install --upgrade virtualenv
+  sudo pip install --upgrade virtualenvwrapper
 elif [ "$(uname -s)" = "Darwin" ]; then
   brew install python3
   pip install --upgrade virtualenv
