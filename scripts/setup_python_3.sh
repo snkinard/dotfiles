7a2e6f7481573c41e0f0355367dfac860ee3b6d7
Sam Kinard <snkinard@gmail.com>
2016-08-08 16:17:47 -0500

    better python install script

--- scripts/setup_python_3.sh
+++ scripts/setup_python_3.sh
@@ -3,12 +3,14 @@
 # install python 3
 if [ "$(uname -s)" = "Linux" ]; then
   sudo apt-get install python3
-  sudo apt-get install pip
+  sudo apt-get install python-pip
+  sudo pip install --upgrade pip
+  sudo pip install --upgrade virtualenv
+  sudo pip install --upgrade virtualenvwrapper
 elif [ "$(uname -s)" = "Darwin" ]; then
   brew install python3
+  pip install --upgrade virtualenv
+  pip install --upgrade virtualenvwrapper
 fi
-
-pip install virtualenv
-pip install virtualenvwrapper
 
 mkdir ~/.virtualenvs
