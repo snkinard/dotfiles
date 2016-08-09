6aa7471474b2ecdafd8fb08074f9a507d674fd88
Sam Kinard <snkinard@gmail.com>
2016-08-09 17:15:16 -0500

    add more casks

--- brew-recipes/update.sh
+++ brew-recipes/update.sh
@@ -24,7 +24,15 @@ brew install shpotify
 brew install task
 
 # cask
-brew install caskroom/cask/brew-cask
+brew tap caskroom/cask
+brew cask install intellij-idea
+brew cask install google-chrome
+brew cask install slack
+brew cask install spotify
+brew cask install flux
+brew cask install dropbox
+brew cask install virtualbox
+brew cask install iterm2
 
 # remove outdated versions from cellar
 brew cleanup
