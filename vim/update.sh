bd742768ced86aacc3397817d289e576c3eca8d4
Sam Kinard <snkinard@gmail.com>
2016-08-12 10:42:21 -0500

    update vim stuff. ideavimrc for idea vim plugin

--- vim/update.sh
+++ vim/update.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 script_dir=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+platform=$(uname)
 
 if [[ ! -a ~/.vimrc ]]
 then
@@ -10,6 +11,15 @@ fi
 if [[ ! -a ~/.vim ]]
 then
   ln -s $script_dir/vim.symlink ~/.vim
+fi
+
+# Idea is only used in OSX
+if [[ $platform == *Darwin* ]]
+then
+  if [[ ! -a ~/.ideavimrc ]] 
+  then 
+    ln -s $script_dir/ideavimrc.symlink ~/.ideavimrc 
+  fi
 fi
 
 vim +PluginInstall +qall
