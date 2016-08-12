75990cad90062dba49c3163b1a17f0fd4d50c121
Sam Kinard <snkinard@gmail.com>
2016-08-12 10:55:58 -0500

    platform check for apt-get

--- apt-get/update.sh
+++ apt-get/update.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
-sudo apt-get update
+platform=$(uname)
+if [ $platform == "Linux" ]; then
+  sudo apt-get update
 
-# core tools
-sudo apt-get install curl
-sudo apt-get install git
-sudo apt-get install zsh
-sudo apt-get install vim
+  # core tools
+  sudo apt-get install curl
+  sudo apt-get install git
+  sudo apt-get install zsh
+  sudo apt-get install vim
+fi
 
